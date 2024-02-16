a5018dc209b15f667683a952eefe870d1e16f90d
Arsénio Costa <arsenio@sketch.com>
2024-02-16 18:16:52 +0000

    test: polish a bit more the tests for 4.4

--- gopl/ch4/4.4/main_test.go
+++ gopl/ch4/4.4/main_test.go
@@ -7,40 +7,65 @@ import (
 
 func TestRotate(t *testing.T) {
 	var tests = []struct {
-		a, acopy []int
+		name     string
+		a        []int
 		n        int
-		want     []int
+		expected []int
 	}{
-
-		{a: []int{1, 2, 3, 4, 5}, acopy: []int{1, 2, 3, 4, 5}, n: 0, want: []int{1, 2, 3, 4, 5}},
-		{a: []int{5, 4, 3, 2, 1}, acopy: []int{5, 4, 3, 2, 1}, n: 0, want: []int{5, 4, 3, 2, 1}},
-		{a: []int{10, 20, 30, 40, 50}, acopy: []int{10, 20, 30, 40, 50}, n: 0, want: []int{10, 20, 30, 40, 50}},
-
-		{a: []int{1, 2, 3, 4, 5}, acopy: []int{1, 2, 3, 4, 5}, n: 1, want: []int{5, 1, 2, 3, 4}},
-		{a: []int{1, 2, 3, 4, 5}, acopy: []int{1, 2, 3, 4, 5}, n: 2, want: []int{4, 5, 1, 2, 3}},
-		{a: []int{5, 4, 3, 2, 1}, acopy: []int{5, 4, 3, 2, 1}, n: 3, want: []int{3, 2, 1, 5, 4}},
-		{a: []int{10, 20, 30, 40, 50}, acopy: []int{10, 20, 30, 40, 50}, n: 4, want: []int{20, 30, 40, 50, 10}},
-
-		{a: []int{1, 2, 3, 4, 5}, acopy: []int{1, 2, 3, 4, 5}, n: -2, want: []int{3, 4, 5, 1, 2}},
-		{a: []int{5, 4, 3, 2, 1}, acopy: []int{5, 4, 3, 2, 1}, n: -3, want: []int{2, 1, 5, 4, 3}},
-		{a: []int{10, 20, 30, 40, 50}, acopy: []int{10, 20, 30, 40, 50}, n: -4, want: []int{50, 10, 20, 30, 40}},
-
-		// n greater than the length of the slice
-		{a: []int{1, 2, 3, 4, 5}, acopy: []int{1, 2, 3, 4, 5}, n: 7, want: []int{4, 5, 1, 2, 3}},
-		{a: []int{5, 4, 3, 2, 1}, acopy: []int{5, 4, 3, 2, 1}, n: 6, want: []int{1, 5, 4, 3, 2}},
-
-		// n multiple of the length of the slice
-		{a: []int{10, 20, 30, 40, 50}, acopy: []int{10, 20, 30, 40, 50}, n: 5, want: []int{10, 20, 30, 40, 50}},
-		{a: []int{10, 20, 30, 40, 50}, acopy: []int{10, 20, 30, 40, 50}, n: 10, want: []int{10, 20, 30, 40, 50}},
-		// n multiple negative of the length of the slice
-		{a: []int{10, 20, 30, 40, 50}, acopy: []int{10, 20, 30, 40, 50}, n: -5, want: []int{10, 20, 30, 40, 50}},
-		{a: []int{10, 20, 30, 40, 50}, acopy: []int{10, 20, 30, 40, 50}, n: -10, want: []int{10, 20, 30, 40, 50}},
+		{
+			name:     "No rotation",
+			a:        []int{1, 2, 3, 4, 5},
+			n:        0,
+			expected: []int{1, 2, 3, 4, 5},
+		},
+		{
+			name:     "Positive rotation",
+			a:        []int{1, 2, 3, 4, 5},
+			n:        1,
+			expected: []int{5, 1, 2, 3, 4},
+		},
+		{
+			name:     "Negative rotation",
+			a:        []int{1, 2, 3, 4, 5},
+			n:        -2,
+			expected: []int{3, 4, 5, 1, 2},
+		},
+		{
+			name:     "Rotation greater than length",
+			a:        []int{1, 2, 3, 4, 5},
+			n:        7,
+			expected: []int{4, 5, 1, 2, 3},
+		},
+		{
+			name:     "Rotation less than negative length",
+			a:        []int{1, 2, 3, 4, 5},
+			n:        -8,
+			expected: []int{4, 5, 1, 2, 3},
+		},
+		{
+			name:     "Rotation multiple of length",
+			a:        []int{1, 2, 3, 4, 5},
+			n:        10,
+			expected: []int{1, 2, 3, 4, 5},
+		},
+		{
+			name:     "Negative rotation multiple of length",
+			a:        []int{1, 2, 3, 4, 5},
+			n:        -15,
+			expected: []int{1, 2, 3, 4, 5},
+		},
 	}
 
 	for _, test := range tests {
-		rotate(test.a, test.n)
-		if !slices.Equal(test.a, test.want) {
-			t.Errorf("reverse(%v, %d) = %v, want %v", test.acopy, test.n, test.a, test.want)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			// Make a copy of the original slice since the rotate function modifies the original slice in place
+			aCopy := make([]int, len(test.a))
+			copy(aCopy, test.a)
+
+			rotate(test.a, test.n)
+			if !slices.Equal(test.a, test.expected) {
+				t.Errorf("rotate(%v, %d) = %v, want %v", aCopy, test.n, test.a, test.expected)
+			}
+		})
 	}
 }
